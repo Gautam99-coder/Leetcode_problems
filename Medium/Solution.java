ab1f448d3e9a9613470a246cc58de3912e8c0645
Gautam99-coder <186465364+Gautam99-coder@users.noreply.github.com>
2025-04-13 23:22:06 +0530

    Create Solution.java

---
+++ Medium/Solution.java
@@ -0,0 +1,26 @@
+class Solution {
+    private static final long MOD = 1000000007;
+
+    public int countGoodNumbers(long n) {
+        long evenCount = (n + 1) / 2;
+        long oddCount = n / 2;
+        
+        long evenWays = powMod(5, evenCount, MOD);
+        long oddWays = powMod(4, oddCount, MOD);
+        
+        return (int) ((evenWays * oddWays) % MOD);
+    }
+    
+    private long powMod(long base, long exponent, long mod) {
+        long result = 1;
+        base %= mod;
+        while (exponent > 0) {
+            if (exponent % 2 == 1) {
+                result = (result * base) % mod;
+            }
+            base = (base * base) % mod;
+            exponent >>= 1;
+        }
+        return result;
+    }
+}
