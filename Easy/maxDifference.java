151a8e624c2cc2eb7a3b1a19e6eca799822570e2
Gautam99-coder <gautamtharu1000@gmail.com>
2025-06-10 22:41:56 +0545

    Create maxDifference.java

---
+++ Easy/maxDifference.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int maxDifference(String s) {
+        // 1) Build frequency table
+        int[] freq = new int[26];
+        for (char c : s.toCharArray()) {
+            freq[c - 'a']++;
+        }
+
+        // 2) Find the largest odd freq and smallest even freq (only for letters that appear)
+        int maxOdd = Integer.MIN_VALUE;
+        int minEven = Integer.MAX_VALUE;
+
+        for (int f : freq) {
+            if (f == 0) continue;        // ignore letters not present
+            if (f % 2 == 1) {           // odd
+                maxOdd = Math.max(maxOdd, f);
+            } else {                    // even
+                minEven = Math.min(minEven, f);
+            }
+        }
+
+        // 3) By problem guarantee, there's at least one odd and one even frequency
+        return maxOdd - minEven;
+    }
+}
